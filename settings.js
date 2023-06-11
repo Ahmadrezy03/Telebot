1202841b70a88616a6b10438a5c4e7a93840bf88
Ahmadrezy03 <120911154+Ahmadrezy03@users.noreply.github.com>
2023-06-11 14:06:24 +0800

    Update settings.js
    oke

--- settings.js
+++ settings.js
@@ -17,11 +17,11 @@ global.APIKeys = {
 //language 
 global.language = indonesia //change indonesia to english if you don't understand the language used by the bot
 
-global.BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN" //create bot here https://t.me/BotFather and get the bot token
-global.BOT_NAME = "Alphabot-Mdོ" //your bot name
-global.OWNER_NAME = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //your name
-global.OWNER_NUMBER = "62887435047326" //your telegram number
+global.BOT_TOKEN = 5913590964:AAFyKdqNF9UslHeLvvI3PqJabnnJH9PZCX0 //create bot here https://t.me/BotFather and get the bot token
+global.BOT_NAME = https://t.me/BOTawal_mint_bot//your bot name
+global.OWNER_NAME = https://t.me/Bangrezy //your name
+global.OWNER_NUMBER = "6285163662471" //your telegram number
 global.OWNER = ["https://t.me/zeeoneofc", "https://t.me/zeeone_ofc"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
-global.THUMBNAIL = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
-global.DONASI = "./image/donasi.jpg" // foto donasi di folder image
+global.THUMBNAIL = C:\Users\Administrator\Pictures\Foto random\52764718_118586405945669_3876272559473295360_n.jpg // ini lol.jpg adalah nama foto di folder image. untuk foto bot
+global.DONASI = // foto donasi di folder image
 global.lang = language //don't change
